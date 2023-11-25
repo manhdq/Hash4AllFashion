0843260b22a88d308a7840c32749d360eb67b78a
dungnq271 <dungnguyen2712002@gmail.com>
2023-11-26 01:07:53 +0700

    Add script to build new dataset

---
+++ scripts/build/build_fashion32.sh
@@ -0,0 +1,3 @@
+python build_dataset.py \
+       --src /home/dungmaster/Datasets/Fashion-Outfits-Theme-Aware/images \
+       --dst /home/dungmaster/Datasets/Fashion-Outfits-Theme-Aware/images_lmdb
