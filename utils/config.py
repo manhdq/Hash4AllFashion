5b3889bfddd01354f75a346c322f7e9cf98cccc1
dungnq271 <dungnguyen2712002@gmail.com>
2023-11-27 13:27:08 +0700

    Update CateName

--- utils/config.py
+++ utils/config.py
@@ -1,27 +1,27 @@
 """Configuration file."""
-NumCate = 7
+NumCate = 5
+
+# CateName = [
+#     "full-body",
+#     "top",
+#     "bottom",
+#     "outerwear",
+#     "bag",
+#     "footwear",
+#     "accessory",
+# ]
+
 CateName = [
-#    "full-body",
     "top",
     "bottom",
     "outerwear",
     "bag",
     "footwear",
-#    "accessory",
 ]
-# CateIdx = {
-#     "full-body": 0,
-#     "bottom": 1,
-#     "top": 2,
-#     "outerwear": 3,
-#     "bag": 4,
-#     "footwear": 5,
-#     "accessory": 6,
-# }
+
 CateIdx = {
     CateName[i]: i for i in range(len(CateName))
 }
-print(CateIdx)
 
 NumPhase = 3
 Phase = ["train", "val", "test"]
